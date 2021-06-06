5aa89225f6e845792b99a340da75d8845f98779d
okadatakuya <zhuoyegangtian018@gmail.com>
2021-06-06 10:56:40 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :password, format: { with: /[a-z]{1,}[\d]{1,}/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name_hurigana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_hurigana, format: { with: /\A[ァ-ヶー－]+\z/ }
 
